5a8cf6d7c1719a57959eeb40d1c9a4ea63d70e6d
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-08-24 10:53:19 +0900

    add index for user:name, message:channel_id

--- db/isubata.sql
+++ db/isubata.sql
@@ -11,7 +11,8 @@ CREATE TABLE user (
 CREATE TABLE image (
   id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(191),
-  data LONGBLOB
+  data LONGBLOB,
+  KEY name_index_on_image(name)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE channel (
@@ -27,7 +28,8 @@ CREATE TABLE message (
   channel_id BIGINT,
   user_id BIGINT,
   content TEXT,
-  created_at DATETIME NOT NULL
+  created_at DATETIME NOT NULL,
+  KEY channel_id_index_on_message(channel_id)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE haveread (
