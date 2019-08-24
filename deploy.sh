cbf5b173af7f58c2ab4abb5450950c626f49f54f
pokotsun <gorigolila@gmail.com>
2019-08-24 02:42:28 +0000

    setting mysqld.conf for slow query

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,11 @@ NOW=`date +'%H-%M-%S'`
 sudo cp $LOGPATH/nginx/access.log $LOGPATH/nginx/access-$NOW.log
 sudo sh -c 'echo "" > /var/log/nginx/access.log'
 
-#sudo cp $LOGPATH/mariadb/slow.log $LOGPATH/mariadb/slow-$NOW.log
-#sudo sh -c 'echo "" > /var/log/mariadb/slow.log'
+sudo cp $LOGPATH/mysql/slow.log $LOGPATH/mysql/slow-$NOW.log
+sudo sh -c 'echo "" > /var/log/mysql/slow.log'
 
 ## replace mysql conf
-#sudo cp conf/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp conf/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 ## restart application services
 ## db, app, nginx, redis
@@ -43,6 +43,6 @@ cd bench
 ./bin/bench -remotes=127.0.0.1 -output result.json
 )
 jq . < bench/result.json
-#sudo /usr/local/bin/alp -f /var/log/nginx/access.log -r --sum | head -n 30
-#sudo mysqldumpslow -s t /var/log/mariadb/slow.log | head -n 30
+sudo /usr/local/bin/alp -f /var/log/nginx/access.log -r --sum | head -n 30
+sudo mysqldumpslow -s t /var/log/mysql/slow.log | head -n 30
 
