072c486dcdb7de07b87b193e95bb33b5501c3ab3
naoto67 <uver.com67@gmail.com>
2019-08-24 10:46:27 +0900

    divide files

---
+++ webapp/go/src/isubata/channel.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+func queryChannels() ([]int64, error) {
+	res := []int64{}
+	err := db.Select(&res, "SELECT id FROM channel")
+	return res, err
+}
+
+func getChannel(c echo.Context) error {
+	user, err := ensureLogin(c)
+	if user == nil {
+		return err
+	}
+	cID, err := strconv.Atoi(c.Param("channel_id"))
+	if err != nil {
+		return err
+	}
+	channels := []ChannelInfo{}
+	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
+	if err != nil {
+		return err
+	}
+
+	var desc string
+	for _, ch := range channels {
+		if ch.ID == int64(cID) {
+			desc = ch.Description
+			break
+		}
+	}
+	return c.Render(http.StatusOK, "channel", map[string]interface{}{
+		"ChannelID":   cID,
+		"Channels":    channels,
+		"User":        user,
+		"Description": desc,
+	})
+}
+
+func getHistory(c echo.Context) error {
+	chID, err := strconv.ParseInt(c.Param("channel_id"), 10, 64)
+	if err != nil || chID <= 0 {
+		return ErrBadReqeust
+	}
+
+	user, err := ensureLogin(c)
+	if user == nil {
+		return err
+	}
+
+	var page int64
+	pageStr := c.QueryParam("page")
+	if pageStr == "" {
+		page = 1
+	} else {
+		page, err = strconv.ParseInt(pageStr, 10, 64)
+		if err != nil || page < 1 {
+			return ErrBadReqeust
+		}
+	}
+
+	const N = 20
+	var cnt int64
+	err = db.Get(&cnt, "SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?", chID)
+	if err != nil {
+		return err
+	}
+	maxPage := int64(cnt+N-1) / N
+	if maxPage == 0 {
+		maxPage = 1
+	}
+	if page > maxPage {
+		return ErrBadReqeust
+	}
+
+	messages := []Message{}
+	err = db.Select(&messages,
+		"SELECT * FROM message WHERE channel_id = ? ORDER BY id DESC LIMIT ? OFFSET ?",
+		chID, N, (page-1)*N)
+	if err != nil {
+		return err
+	}
+
+	mjson := make([]map[string]interface{}, 0)
+	for i := len(messages) - 1; i >= 0; i-- {
+		r, err := jsonifyMessage(messages[i])
+		if err != nil {
+			return err
+		}
+		mjson = append(mjson, r)
+	}
+
+	channels := []ChannelInfo{}
+	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
+	if err != nil {
+		return err
+	}
+
+	return c.Render(http.StatusOK, "history", map[string]interface{}{
+		"ChannelID": chID,
+		"Channels":  channels,
+		"Messages":  mjson,
+		"MaxPage":   maxPage,
+		"Page":      page,
+		"User":      user,
+	})
+}
+
+func getAddChannel(c echo.Context) error {
+	self, err := ensureLogin(c)
+	if self == nil {
+		return err
+	}
+
+	channels := []ChannelInfo{}
+	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
+	if err != nil {
+		return err
+	}
+
+	return c.Render(http.StatusOK, "add_channel", map[string]interface{}{
+		"ChannelID": 0,
+		"Channels":  channels,
+		"User":      self,
+	})
+}
+
+func postAddChannel(c echo.Context) error {
+	self, err := ensureLogin(c)
+	if self == nil {
+		return err
+	}
+
+	name := c.FormValue("name")
+	desc := c.FormValue("description")
+	if name == "" || desc == "" {
+		return ErrBadReqeust
+	}
+
+	res, err := db.Exec(
+		"INSERT INTO channel (name, description, updated_at, created_at) VALUES (?, ?, NOW(), NOW())",
+		name, desc)
+	if err != nil {
+		return err
+	}
+	lastID, _ := res.LastInsertId()
+	return c.Redirect(http.StatusSeeOther,
+		fmt.Sprintf("/channel/%v", lastID))
+}
